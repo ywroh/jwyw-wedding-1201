a63cde7eb27bc4cf86717719a3a93b8e3443519f
YongWoo Roh <ywroh2@gmail.com>
2024-09-12 18:02:59 +0900

    add : assetPrefix 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    assetPrefix: process.env.NODE_ENV !== 'production' ? undefined : 'https://ywroh.github.io/jwyw-wedding-1201/',
+};
 
 export default nextConfig;
